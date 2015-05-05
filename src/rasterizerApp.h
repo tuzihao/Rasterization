4c2e895c9cc3a477551cb14edeca5a29c1787407
boris <boris@borisdeMacBook-Pro.local>
2015-05-05 15:06:43 +0800

    -A

--- src/rasterizerApp.h
+++ src/rasterizerApp.h
@@ -8,17 +8,15 @@
 
 class rasterizerApp {
 public:
-    rasterizerApp(HINSTANCE hinstance) { hInst = hinstance; }
-    ~rasterizerApp();
+    rasterizerApp(HINSTANCE hinstance) { hInst = hinstance; };
+    vritual ~rasterizerApp();
     
-    HINSTANCE appInst()const;
-    HWND mainWnd() const;
     
     bool initMainWindow();
     int run();
     void render();
 
-    LRESULT  MsgProc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    LRESULT  MsgProc (HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 private:
     HINSTANCE hInst;
